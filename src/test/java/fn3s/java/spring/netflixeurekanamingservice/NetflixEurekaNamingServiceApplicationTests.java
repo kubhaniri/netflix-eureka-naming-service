6949dc78ca0560059a29f3f79456295d05960acf
Firmin Sosso Ngoutte <firmin.sossongoutte@soprasteria.com>
2020-01-17 10:32:25 +0100

    Initial Commit

---
+++ src/test/java/fn3s/java/spring/netflixeurekanamingservice/NetflixEurekaNamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fn3s.java.spring.netflixeurekanamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
