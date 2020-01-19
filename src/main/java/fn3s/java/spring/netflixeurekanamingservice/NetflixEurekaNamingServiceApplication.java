20ada3b3b3b90d87ead6fdfe40974e61eb7ed8ab
kubha <firmsong@fn3s.com>
2020-01-19 22:10:11 +0100

    Generai Update

--- src/main/java/fn3s/java/spring/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
+++ src/main/java/fn3s/java/spring/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
@@ -1,8 +1,10 @@
 package fn3s.java.spring.netflixeurekanamingservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,5 +13,8 @@ public class NetflixEurekaNamingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixEurekaNamingServiceApplication.class, args);
 	}
-
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
--- src/main/java/fn3s/java/spring/netflixeurekanamingservice/config/Configuration.java
+++ src/main/java/fn3s/java/spring/netflixeurekanamingservice/config/Configuration.java
@@ -1,19 +1,23 @@
 package fn3s.java.spring.netflixeurekanamingservice.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("netflix-eureka-naming-service")
 public class Configuration {
-
+    private Logger log = LoggerFactory.getLogger(this.getClass());
     private String name;
 
     public String getName() {
+        log.info("getName name: {}",name);
         return name;
     }
 
     public void setName(String name) {
+        log.info("getName name: {}",name);
         this.name = name;
     }
 }
